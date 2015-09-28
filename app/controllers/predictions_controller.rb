0cc59f53b75c5760db41082f80f6b4b9be2c5a49
Ryan Kulp <ryanckulp@gmail.com>
2015-09-28 14:24:17 +0700

    seo

--- app/controllers/predictions_controller.rb
+++ app/controllers/predictions_controller.rb
@@ -1,11 +1,6 @@
 class PredictionsController < ApplicationController
   before_action :set_prediction, only: [:show, :edit, :update, :destroy]
 
-  # GET /predictions
-  def index
-    @predictions = Prediction.all
-  end
-
   def show
     # default handle if none given by user
       @prediction.handle = "yourhandle" if @prediction.handle.empty?
@@ -39,10 +34,6 @@ class PredictionsController < ApplicationController
     @prediction = Prediction.new
   end
 
-  # GET /predictions/1/edit
-  def edit
-  end
-
   # POST /predictions
   def create
     @prediction = Prediction.new(prediction_params)
@@ -55,28 +46,6 @@ class PredictionsController < ApplicationController
         format.html { render :new }
         format.json { render json: @prediction.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /predictions/1
-  def update
-    respond_to do |format|
-      if @prediction.update(prediction_params)
-        format.html { redirect_to @prediction, notice: 'Prediction was successfully updated.' }
-        format.json { render :show, status: :ok, location: @prediction }
-      else
-        format.html { render :edit }
-        format.json { render json: @prediction.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /predictions/1
-  def destroy
-    @prediction.destroy
-    respond_to do |format|
-      format.html { redirect_to predictions_url, notice: 'Prediction was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
